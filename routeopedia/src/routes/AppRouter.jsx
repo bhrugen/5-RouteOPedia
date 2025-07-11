cf2fca619a0a0c2a844ba9dea6c9abb31f93d3d7
bhrugen <bhrugen1990@gmail.com>
2025-07-11 14:55:33 -0500

    6 Dynamic navigation

--- routeopedia/src/routes/AppRouter.jsx
+++ routeopedia/src/routes/AppRouter.jsx
@@ -3,12 +3,14 @@ import Home from "../pages/Home";
 import Contact from "../pages/Contact";
 import NotFound from "../pages/NotFound";
 import ProductList from "../pages/product/ProductList";
+import ProductDetail from "../pages/product/ProductDetail";
 function AppRouter() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/contact" element={<Contact />} />
       <Route path="/products" element={<ProductList />} />
+      <Route path="/products/item/:id" element={<ProductDetail />} />
       <Route path="*" element={<NotFound />} />
     </Routes>
   );
